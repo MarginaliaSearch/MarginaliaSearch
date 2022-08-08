0f59675f7c5c405346212e790b4d1e3834981e66
vlofgren <vlofgren@gmail.com>
2022-08-08 18:08:18 +0200

    Clean up preconverter code

--- marginalia_nu/src/test/java/nu/marginalia/wmsa/edge/search/query/QueryVariantsTest.java
+++ marginalia_nu/src/test/java/nu/marginalia/wmsa/edge/search/query/QueryVariantsTest.java
@@ -1,13 +1,11 @@
 package nu.marginalia.wmsa.edge.search.query;
 
 import nu.marginalia.util.TestLanguageModels;
-import nu.marginalia.wmsa.edge.assistant.dict.NGramDict;
 import nu.marginalia.util.language.conf.LanguageModels;
 import nu.marginalia.util.language.processing.SentenceExtractor;
+import nu.marginalia.wmsa.edge.assistant.dict.NGramDict;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 class QueryVariantsTest {
     QueryVariants variants;
@@ -25,30 +23,36 @@ class QueryVariantsTest {
         parser = new QueryParser(new EnglishDictionary(dict), variants);
     }
 
-    @Test @SuppressWarnings("unchecked")
-    void getQueryVariants() {
+    @Test
+    void getQueryFood() {
         System.out.println(se.extractSentence("we are alone"));
-        testCase("DOS", List.of("DOS"));
-        testCase("dos", List.of("dos"));
-        testCase("we are alone", List.of("dos"));
-        testCase("3D Realms", List.of("dos"));
-        testCase("I am alone", List.of("dos"));
-        testCase("plato cave", List.of("dos"));
-        testCase("The internet is dead", List.of("dos"));
-
-        testCase("TRS80", List.of("trs_80"), List.of("trs80"));
-        testCase("TRS-80", List.of("trs-80"), List.of("trs80"));
-        testCase("TRS-80", List.of("trs-80"), List.of("trs80"));
-        testCase("Raspberry Pi 2", List.of("trs-80"), List.of("trs80"));
-        testCase("Duke Nukem 3D", List.of("trs-80"), List.of("trs80"));
-        testCase("The Man of Tomorrow", List.of("trs-80"), List.of("trs80"));
-        testCase("Computer Manual", List.of("trs-80"), List.of("trs80"));
-        testCase("Knitting", List.of("trs-80"), List.of("trs80"));
-        testCase("capcom", List.of("trs-80"), List.of("trs80"));
-        testCase("the man of tomorrow", List.of("trs-80"), List.of("trs80"));
+        testCase("Omelet recipe");
     }
 
-    private void testCase(String input, List<String>... expected) {
+    @Test
+    void getQueryVariants() {
+        System.out.println(se.extractSentence("we are alone"));
+        testCase("DOS");
+        testCase("dos");
+        testCase("we are alone");
+        testCase("3D Realms");
+        testCase("I am alone");
+        testCase("plato cave");
+        testCase("The internet is dead");
+
+        testCase("TRS80");
+        testCase("TRS-80");
+        testCase("TRS-80");
+        testCase("Raspberry Pi 2");
+        testCase("Duke Nukem 3D");
+        testCase("The Man of Tomorrow");
+        testCase("Computer Manual");
+        testCase("Knitting");
+        testCase("capcom");
+        testCase("the man of tomorrow");
+    }
+
+    private void testCase(String input) {
         var tokens = variants.getQueryVariants(parser.extractBasicTokens(input));
         System.out.println(tokens);
     }
