bdd2b4a43e9b7459865d8ddae03cf5ebb6221b5a
Viktor Lofgren <vlofgren@marginalia.nu>
2023-03-19 11:46:13 +0100

    Put news into a database table instead of keeping them hardcoded.

--- code/services-core/search-service/src/main/java/nu/marginalia/search/command/IndexCommand.java
+++ code/services-core/search-service/src/main/java/nu/marginalia/search/command/IndexCommand.java
@@ -2,28 +2,62 @@ package nu.marginalia.search.command;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import nu.marginalia.browse.model.BrowseResultSet;
+import com.zaxxer.hikari.HikariDataSource;
 import nu.marginalia.renderer.MustacheRenderer;
 import nu.marginalia.renderer.RendererFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Singleton
 public class IndexCommand {
 
-    private final MustacheRenderer<BrowseResultSet> template;
-    @Inject
-    public IndexCommand(RendererFactory rendererFactory) throws IOException {
+    private final MustacheRenderer<IndexModel> template;
+    private final HikariDataSource dataSource;
 
-        template = rendererFactory.renderer("search/index");
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Inject
+    public IndexCommand(RendererFactory rendererFactory, HikariDataSource dataSource) throws IOException {
+        this.template = rendererFactory.renderer("search/index/index");
+        this.dataSource = dataSource;
     }
 
     public String render(Request request, Response response) {
         response.header("Cache-control", "public,max-age=3600");
 
-        return template.render(new BrowseResultSet(Collections.emptyList()));
+        return template.render(new IndexModel(getNewsItems()));
     }
+
+
+    private List<NewsItem> getNewsItems() {
+        List<NewsItem> items = new ArrayList<>();
+
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement("""
+                SELECT TITLE, LINK, LIST_DATE FROM SEARCH_NEWS_FEED
+                """)) {
+
+            var rep = stmt.executeQuery();
+
+            while (rep.next()) {
+                items.add(new NewsItem(rep.getString(1), rep.getString(2), rep.getDate(3).toLocalDate()));
+            }
+        }
+        catch (SQLException ex) {
+            logger.warn("Failed to fetch news items", ex);
+        }
+
+        return items;
+    }
+
+    private record IndexModel(List<NewsItem> news) { }
+    private record NewsItem(String title, String url, LocalDate date) {}
 }
