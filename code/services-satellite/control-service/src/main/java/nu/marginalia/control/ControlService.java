7a17933c65252a59ee4a7a993c2c48c421dfca72
Viktor Lofgren <vlofgren@marginalia.nu>
2023-07-04 19:52:30 +0200

    Control service owns message queue garbage collection.

--- code/services-satellite/control-service/src/main/java/nu/marginalia/control/ControlService.java
+++ code/services-satellite/control-service/src/main/java/nu/marginalia/control/ControlService.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.inject.Inject;
 import nu.marginalia.client.ServiceMonitors;
 import nu.marginalia.model.gson.GsonFactory;
+import nu.marginalia.mq.persistence.MqPersistence;
 import nu.marginalia.renderer.MustacheRenderer;
 import nu.marginalia.renderer.RendererFactory;
 import nu.marginalia.service.server.*;
@@ -13,6 +14,7 @@ import spark.Spark;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ControlService extends Service {
 
@@ -22,6 +24,7 @@ public class ControlService extends Service {
     private final ServiceMonitors monitors;
     private final MustacheRenderer<Object> indexRenderer;
     private final MustacheRenderer<Map<?,?>> servicesRenderer;
+    private final MqPersistence messageQueuePersistence;
 
 
     @Inject
@@ -29,13 +32,15 @@ public class ControlService extends Service {
                           ServiceMonitors monitors,
                           HeartbeatService heartbeatService,
                           EventLogService eventLogService,
-                          RendererFactory rendererFactory
+                          RendererFactory rendererFactory,
+                          MqPersistence messageQueuePersistence
                       ) throws IOException {
 
         super(params);
         this.monitors = monitors;
         indexRenderer = rendererFactory.renderer("control/index");
         servicesRenderer = rendererFactory.renderer("control/services");
+        this.messageQueuePersistence = messageQueuePersistence;
 
         Spark.get("/public/heartbeats", (req, res) -> {
             res.type("application/json");
@@ -50,7 +55,30 @@ public class ControlService extends Service {
 
         monitors.subscribe(this::logMonitorStateChange);
 
+        Thread reaperThread = new Thread(this::reapMessageQueue, "message-queue-reaper");
+        reaperThread.setDaemon(true);
+        reaperThread.start();
+    }
 
+    private void reapMessageQueue() {
+
+        for (;;) {
+            try {
+                TimeUnit.MINUTES.sleep(30);
+
+                int outcome = messageQueuePersistence.reapDeadMessages();
+                if (outcome > 0) {
+                    logger.info("Reaped {} dead messages from message queue", outcome);
+                }
+            }
+            catch (InterruptedException ex) {
+                logger.info("Message queue reaper interrupted");
+                return;
+            }
+            catch (Exception ex) {
+                logger.error("Message queue reaper failed", ex);
+            }
+        }
     }
 
     private void logMonitorStateChange() {
