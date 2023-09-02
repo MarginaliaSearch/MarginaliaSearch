676e7c79472c2fba667f8dc91d965b38f99a3133
Viktor Lofgren <vlofgren@marginalia.nu>
2023-09-02 09:52:01 +0200

    (keywords) Add Serializable properties that went missing as the record became a class

--- code/features-convert/keyword-extraction/src/main/java/nu/marginalia/keyword/model/DocumentKeywords.java
+++ code/features-convert/keyword-extraction/src/main/java/nu/marginalia/keyword/model/DocumentKeywords.java
@@ -1,11 +1,22 @@
 package nu.marginalia.keyword.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import nu.marginalia.model.idx.WordMetadata;
 
-public final class DocumentKeywords {
-    final String[] keywords;
-    final long[] metadata;
+import java.io.Serial;
+import java.io.Serializable;
+
+public final class DocumentKeywords implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1387282293082091432L;
+
+    public final String[] keywords;
+    public final long[] metadata;
 
     public DocumentKeywords(String[] keywords,
                             long[] metadata)
