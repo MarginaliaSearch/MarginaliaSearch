97d43a6fa2481ceec13ad7328c5f24f8da67427f
Viktor Lofgren <vlofgren@gmail.com>
2023-11-27 17:14:58 +0100

    (search) Revamp browse results with new look.

--- code/features-search/random-websites/src/main/java/nu/marginalia/browse/model/BrowseResult.java
+++ code/features-search/random-websites/src/main/java/nu/marginalia/browse/model/BrowseResult.java
@@ -11,4 +11,20 @@ public record BrowseResult (EdgeUrl url, int domainId, double relatedness) {
         }
         return domain.toString();
     }
+
+    public String displayDomain() {
+        String ret;
+        var domain = url.domain;
+        if ("www".equals(domain.subDomain)) {
+            ret = domain.domain;
+        }
+        else {
+            ret = domain.toString();
+        }
+        if (ret.length() > 25) {
+            ret = ret.substring(0, 22) + "...";
+        }
+        return ret;
+
+    }
 }
