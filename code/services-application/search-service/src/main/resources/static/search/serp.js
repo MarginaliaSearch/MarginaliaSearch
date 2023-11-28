38d20022ad814bb0dcf335e6322b6862bc6e1845
Viktor Lofgren <vlofgren@gmail.com>
2023-11-28 14:08:18 +0100

    (search) Fix script loading for mobile support

--- code/services-application/search-service/src/main/resources/static/search/serp.js
+++ code/services-application/search-service/src/main/resources/static/search/serp.js
@@ -2,7 +2,7 @@ document.getElementsByTagName('body')[0].setAttribute('data-has-js', 'true');
 
 // Add a button to open the filters for mobile; we do this in js to not pollute the DOM for text-only browsers
 const button = document.createElement('button');
-button.setAttribute('id', 'mcfest');
+button.setAttribute('id', 'mcfeast');
 button.setAttribute('aria-controls', '#filters');
 button.onclick = (event) => {
     setDisplay(document.getElementById('filters'), 'block');
