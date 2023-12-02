b89633ae4bcab4a9ec84fb0fdff269641321e5d9
Viktor Lofgren <vlofgren@gmail.com>
2023-12-02 17:23:45 +0100

    (search)  Don't render a filter button on mobile when there are no filters to be presented.

--- code/services-application/search-service/src/main/resources/static/search/menu.js
+++ code/services-application/search-service/src/main/resources/static/search/menu.js
@@ -1,7 +1,3 @@
-// This sets the data-has-js attribute on the body tag to true, so we can style the page with the assumption that
-// the browser supports JS. This is a progressive enhancement, so the page will still work without JS.
-document.getElementsByTagName('body')[0].setAttribute('data-has-js', 'true');
-
 const registerCloseButton = () => {
     // Add a button to close the filters for mobile; we do this in js to not pollute the DOM for text-only browsers
     const closeButton = document.createElement('button');
@@ -35,13 +31,3 @@ filtersButton.onclick = (event) => {
 }
 filtersButton.innerHTML = 'Filters';
 document.getElementById('search-box').getElementsByTagName('h1')[0].append(filtersButton);
-
-// To prevent the filter menu from being opened when the user hits enter on the search box, we need to add a keydown
-// handler to the search box that stops the event from propagating.  Janky hack, but it works.
-document.getElementById('query').addEventListener('keydown', e=> {
-    if (e.key === "Enter") {
-        const form = document.getElementById('search-form');
-        form.submit();
-        e.preventDefault();
-    }
-});
