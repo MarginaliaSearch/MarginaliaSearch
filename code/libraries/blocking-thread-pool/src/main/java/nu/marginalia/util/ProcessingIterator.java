ba8a75c84bf1d182b136e74106792f09e9e5df26
Viktor Lofgren <vlofgren@marginalia.nu>
2023-12-29 15:10:32 +0100

    Fix bug in ProcessingIterator where it would run the tasks in only one single thread instead of using the pool

--- code/libraries/blocking-thread-pool/src/main/java/nu/marginalia/util/ProcessingIterator.java
+++ code/libraries/blocking-thread-pool/src/main/java/nu/marginalia/util/ProcessingIterator.java
@@ -38,7 +38,7 @@ public class ProcessingIterator<T> implements Iterator<T> {
 
     private void executeJob(ProcessingJob<T> job) {
         try {
-            job.run(j -> executorService.submit(() -> executeTask(j)));
+            job.run(this::executeTask);
         } catch (Exception e) {
             logger.warn("Exception while processing", e);
         } finally {
@@ -53,13 +53,15 @@ public class ProcessingIterator<T> implements Iterator<T> {
             return;
         }
 
-        try {
-            queue.put(task.get());
-        } catch (Exception e) {
-            logger.warn("Exception while processing", e);
-        } finally {
-            sem.release();
-        }
+        executorService.submit(() -> {
+            try {
+                queue.put(task.get());
+            } catch (Exception e) {
+                logger.warn("Exception while processing", e);
+            } finally {
+                sem.release();
+            }
+        });
     }
 
     /** Returns true if there are more documents to be processed.
