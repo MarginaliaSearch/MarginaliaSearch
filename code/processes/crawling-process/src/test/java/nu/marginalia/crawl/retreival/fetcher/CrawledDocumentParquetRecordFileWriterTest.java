93a2d5afbf9f1bfdd7e4779992af36276ee453c4
Viktor Lofgren <vlofgren@marginalia.nu>
2024-02-01 20:08:15 +0100

    (*) Fix poorly named test
    Likely old refactoring gore.

--- code/processes/crawling-process/src/test/java/nu/marginalia/crawl/retreival/fetcher/CrawledDocumentParquetRecordFileWriterTest.java
+++ code/processes/crawling-process/src/test/java/nu/marginalia/crawl/retreival/fetcher/CrawledDocumentParquetRecordFileWriterTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class CrawledDocumentParquetRecordFileWriterTest {
+public class CrawledDocumentParquetRecordFileWriterTest {
 
     @Test
     public void testXRobotsTag() {
