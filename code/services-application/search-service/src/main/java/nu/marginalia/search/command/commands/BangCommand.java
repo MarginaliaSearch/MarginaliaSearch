9d7df878867e6ec9d91fc7c263dae4d86067ad95
Conor Flynn <conor-f@users.noreply.github.com>
2024-02-08 13:28:02 +0100

    (search) Fix broken !ddg handling
    https://duckduckgo.com/search?q=asdf leads to running a search for the term "search" instead of "asdf".
    
    Both https://duckduckgo.com/<query> and https://duckduckgo.com/?q=<query> are accepted, but using GET vars seemed more in-keeping with the code.

--- code/services-application/search-service/src/main/java/nu/marginalia/search/command/commands/BangCommand.java
+++ code/services-application/search-service/src/main/java/nu/marginalia/search/command/commands/BangCommand.java
@@ -20,7 +20,7 @@ public class BangCommand implements SearchCommandInterface {
     public BangCommand()
     {
         bangsToPattern.put("!g", "https://www.google.com/search?q=%s");
-        bangsToPattern.put("!ddg", "https://duckduckgo.com/search?q=%s");
+        bangsToPattern.put("!ddg", "https://duckduckgo.com/?q=%s");
         bangsToPattern.put("!w", "https://search.marginalia.nu/search?query=%s+site:en.wikipedia.org&profile=wiki");
     }
 
