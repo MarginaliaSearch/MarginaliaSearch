2d076cbd6720feb9454eac247865c598a30eab32
Sam Storment <sjstorment22@gmail.com>
2024-06-05 18:20:33 -0500

    (search) move data-has-js attribute from body to html element

--- code/services-application/search-service/resources/static/search/main.js
+++ code/services-application/search-service/resources/static/search/main.js
@@ -1,6 +1,6 @@
-// This sets the data-has-js attribute on the body tag to true, so we can style the page with the assumption that
+// This sets the data-has-js attribute on the html tag to true, so we can style the page with the assumption that
 // the browser supports JS. This is a progressive enhancement, so the page will still work without JS.
-document.getElementsByTagName('body')[0].setAttribute('data-has-js', 'true');
+document.documentElement.setAttribute('data-has-js', 'true');
 
 // To prevent the filter menu from being opened when the user hits enter on the search box, we need to add a keydown
 // handler to the search box that stops the event from propagating.  Janky hack, but it works.
