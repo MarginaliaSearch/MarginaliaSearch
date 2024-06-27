0e4dd3d76d2832618760c6af6ae4a5e32956eec3
Viktor Lofgren <vlofgren@marginalia.nu>
2024-06-27 13:40:53 +0200

    (minor) Remove accidentally committed debug printf

--- code/index/java/nu/marginalia/index/model/SearchParameters.java
+++ code/index/java/nu/marginalia/index/model/SearchParameters.java
@@ -71,8 +71,6 @@ public class SearchParameters {
         this.budget = new IndexSearchBudget(limits.timeoutMs() / 2);
         this.query = IndexProtobufCodec.convertRpcQuery(request.getQuery());
 
-        System.out.println(query);
-
         this.limitByDomain = limits.resultsByDomain();
         this.limitTotal = limits.resultsTotal();
 
