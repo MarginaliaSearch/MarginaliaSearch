e9f71ee39bfe3bf5cf585f5b540eb39e5240a90e
Viktor Lofgren <vlofgren@marginalia.nu>
2025-01-01 15:23:25 +0100

    (search) Move linked/similar domains to a popover style menu on mobile

--- code/services-application/search-service/resources/static/js/mobile-button.js
+++ code/services-application/search-service/resources/static/js/mobile-button.js
@@ -1,6 +1,14 @@
 document.getElementById("filter-button").addEventListener("click", function() {
     document.getElementById("mobile-menu").classList.toggle("hidden");
 });
-document.getElementById("hide-filter-button").addEventListener("click", function() {
-    document.getElementById("mobile-menu").classList.toggle("hidden");
+
+document.querySelectorAll("#hide-filter-button").forEach(button => {
+    button.addEventListener("click", function() {
+        document.getElementById("mobile-menu").classList.toggle("hidden");
+    });
+});
+document.querySelectorAll(".hide-filter-button").forEach(button => {
+    button.addEventListener("click", function() {
+        document.getElementById("mobile-menu").classList.toggle("hidden");
+    });
 });
