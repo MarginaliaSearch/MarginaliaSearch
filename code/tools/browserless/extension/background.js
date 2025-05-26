c3afc82dad33fd4dbc1427dad145d3510ddafad6
Viktor Lofgren <vlofgren@marginalia.nu>
2025-05-26 13:13:11 +0200

    (website-capture) Rename scripts to be more consistent with extension terminology

--- code/tools/browserless/extension/background.js
+++ code/tools/browserless/extension/background.js
@@ -2,7 +2,7 @@ chrome.webNavigation.onCommitted.addListener(function(details) {
     if (details.transitionType === 'reload' || details.transitionType === 'auto_subframe') {
         chrome.scripting.registerContentScripts([{
             matches : [ "<all_urls>" ],
-            js : [ "inject.js" ],
+            js : [ "content.js" ],
         }]);
     }
 });
