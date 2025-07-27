838a5626eca475e96db0261113c5cb41a3c6435c
Viktor Lofgren <vlofgren@marginalia.nu>
2025-07-27 21:42:04 +0200

    (index) Reduce query buffer size

--- code/index/java/nu/marginalia/index/IndexQueryExecution.java
+++ code/index/java/nu/marginalia/index/IndexQueryExecution.java
@@ -76,7 +76,7 @@ public class IndexQueryExecution {
     }
 
     private void lookup(IndexQuery query) {
-        final LongQueryBuffer buffer = new LongQueryBuffer(1024);
+        final LongQueryBuffer buffer = new LongQueryBuffer(512);
         try {
             while (query.hasMore() && budget.hasTimeLeft()) {
 
