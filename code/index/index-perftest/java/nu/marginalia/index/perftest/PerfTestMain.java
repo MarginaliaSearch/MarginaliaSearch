c71fc46f04751682fedb0b116703324a443da2fc
Viktor Lofgren <vlofgren@marginalia.nu>
2025-07-27 15:22:07 +0200

    (perf) Update perf test with execution scenario

--- code/index/index-perftest/java/nu/marginalia/index/perftest/PerfTestMain.java
+++ code/index/index-perftest/java/nu/marginalia/index/perftest/PerfTestMain.java
@@ -10,6 +10,7 @@ import nu.marginalia.array.page.LongQueryBuffer;
 import nu.marginalia.functions.searchquery.QueryFactory;
 import nu.marginalia.functions.searchquery.query_parser.QueryExpansion;
 import nu.marginalia.index.FullReverseIndexReader;
+import nu.marginalia.index.IndexQueryExecution;
 import nu.marginalia.index.PrioReverseIndexReader;
 import nu.marginalia.index.forward.ForwardIndexReader;
 import nu.marginalia.index.index.CombinedIndexReader;
@@ -39,11 +40,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PerfTestMain {
-    static Duration warmupTime = Duration.ofMinutes(2);
+    static Duration warmupTime = Duration.ofMinutes(1);
     static Duration runTime = Duration.ofMinutes(10);
 
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 4) {
             System.err.println("Arguments: home-dir index-dir query");
             System.exit(255);
         }
@@ -55,12 +56,17 @@ public class PerfTestMain {
                 System.exit(255);
             }
             Path homeDir = Paths.get(args[1]);
-            String query = args[2];
+            String scenario = args[2];
+            String query = args[3];
 
-            run(indexDir, homeDir, query);
+            switch (scenario) {
+                case "valuation" -> runValuation(indexDir, homeDir, query);
+                case "lookup" -> runLookup(indexDir, homeDir, query);
+                case "execution" -> runExecution(indexDir, homeDir, query);
+            }
         }
         catch (NumberFormatException e) {
-            System.err.println("Arguments: data-dir  index-dir query");
+            System.err.println("Arguments: data-dir index-dir query");
             System.exit(255);
         }
         catch (Exception ex) {
@@ -110,9 +116,9 @@ public class PerfTestMain {
         );
     }
 
-    public static void run(Path homeDir,
-                           Path indexDir,
-                           String rawQuery) throws IOException, SQLException
+    public static void runValuation(Path homeDir,
+                                    Path indexDir,
+                                    String rawQuery) throws IOException, SQLException
     {
 
         CombinedIndexReader indexReader = createCombinedIndexReader(indexDir);
@@ -188,5 +194,139 @@ public class PerfTestMain {
         System.out.println("Warmup sum: " + sum);
         System.out.println("Main sum: " + sum2);
         System.out.println(docIds.size());
+    }
+
+    public static void runExecution(Path homeDir,
+                                    Path indexDir,
+                                    String rawQuery) throws IOException, SQLException, InterruptedException {
+
+        CombinedIndexReader indexReader = createCombinedIndexReader(indexDir);
+        QueryFactory queryFactory = createQueryFactory(homeDir);
+        IndexResultRankingService rankingService = createIndexResultRankingService(indexDir, indexReader);
+
+        var queryLimits = RpcQueryLimits.newBuilder()
+                .setTimeoutMs(50)
+                .setResultsTotal(1000)
+                .setResultsByDomain(10)
+                .setFetchSize(4096)
+                .build();
+        SearchSpecification parsedQuery = queryFactory.createQuery(new QueryParams(rawQuery, queryLimits, "NONE", NsfwFilterTier.OFF), PrototypeRankingParameters.sensibleDefaults()).specs;
+
+        System.out.println("Query compiled to: " + parsedQuery.query.compiledQuery);
+
+        SearchParameters searchParameters = new SearchParameters(parsedQuery, new SearchSetAny());
+
+        System.out.println("Running warmup loop!");
+        int sum = 0;
+
+        Instant runEndTime = Instant.now().plus(warmupTime);
+
+        int iter;
+        for (iter = 0;; iter++) {
+            var execution = new IndexQueryExecution(searchParameters, rankingService, indexReader);
+            execution.run();
+            sum += execution.itemsProcessed();
+            if ((iter % 100) == 0 && Instant.now().isAfter(runEndTime)) {
+                break;
+            }
+        }
+        System.out.println("Warmup complete after " + iter + " iters!");
+
+        runEndTime = Instant.now().plus(runTime);
+        Instant runStartTime =  Instant.now();
+        int sum2 = 0;
+        List<Integer> counts = new ArrayList<>();
+        for (iter = 0;; iter++) {
+            var execution = new IndexQueryExecution(searchParameters, rankingService, indexReader);
+            execution.run();
+            sum2 += execution.itemsProcessed();
+            counts.add(execution.itemsProcessed());
+
+            if ((iter % 100) == 0) {
+                if (Instant.now().isAfter(runEndTime)) {
+                    break;
+                }
+                System.out.println(Duration.between(runStartTime, Instant.now()).toMillis() / 1000. + " best times: " +  counts.stream().mapToInt(Integer::intValue).map(i -> -i).sorted().map(i -> -i).limit(3).average().orElse(-1));
+            }
+        }
+        System.out.println("Benchmark complete after " + iter + " iters!");
+        System.out.println("Best counts times: " + counts.stream().mapToInt(Integer::intValue).map(i -> -i).sorted().map(i -> -i).limit(3).average().orElse(-1));
+        System.out.println("Warmup sum: " + sum);
+        System.out.println("Main sum: " + sum2);
+    }
+
+    public static void runLookup(Path homeDir,
+                                    Path indexDir,
+                                    String rawQuery) throws IOException, SQLException
+    {
+
+        CombinedIndexReader indexReader = createCombinedIndexReader(indexDir);
+        QueryFactory queryFactory = createQueryFactory(homeDir);
+
+        var queryLimits = RpcQueryLimits.newBuilder()
+                .setTimeoutMs(10_000)
+                .setResultsTotal(1000)
+                .setResultsByDomain(10)
+                .setFetchSize(4096)
+                .build();
+        SearchSpecification parsedQuery = queryFactory.createQuery(new QueryParams(rawQuery, queryLimits, "NONE", NsfwFilterTier.OFF), PrototypeRankingParameters.sensibleDefaults()).specs;
+
+        System.out.println("Query compiled to: " + parsedQuery.query.compiledQuery);
+
+        SearchParameters searchParameters = new SearchParameters(parsedQuery, new SearchSetAny());
+
+
+        Instant runEndTime = Instant.now().plus(warmupTime);
+
+        LongQueryBuffer buffer = new LongQueryBuffer(4096);
+        int sum1 = 0;
+        int iter;
+        for (iter = 0;; iter++) {
+            List<IndexQuery> queries = indexReader.createQueries(new SearchTerms(searchParameters.query, searchParameters.compiledQueryIds), searchParameters.queryParams);
+
+            for (var query : queries) {
+                while (query.hasMore()) {
+                    query.getMoreResults(buffer);
+                    sum1 += buffer.end;
+                    buffer.reset();
+                }
+            }
+
+            if ((iter % 100) == 0 && Instant.now().isAfter(runEndTime)) {
+                break;
+            }
+        }
+
+        System.out.println("Warmup complete after " + iter + " iters with sum1 = " + sum1);
+
+        runEndTime = Instant.now().plus(runTime);
+        Instant runStartTime =  Instant.now();
+        int sum2 = 0;
+        List<Double> times = new ArrayList<>();
+        for (iter = 0;; iter++) {
+            List<IndexQuery> queries = indexReader.createQueries(new SearchTerms(searchParameters.query, searchParameters.compiledQueryIds), searchParameters.queryParams);
+
+            long start = System.nanoTime();
+            for (var query : queries) {
+                while (query.hasMore()) {
+                    query.getMoreResults(buffer);
+                    sum1 += buffer.end;
+                    buffer.reset();
+                }
+            }
+            long end = System.nanoTime();
+            times.add((end - start)/1_000_000.);
+
+            if ((iter % 100) == 0) {
+                if (Instant.now().isAfter(runEndTime)) {
+                    break;
+                }
+                System.out.println(Duration.between(runStartTime, Instant.now()).toMillis() / 1000. + " best times: " + times.stream().mapToDouble(Double::doubleValue).sorted().limit(3).average().orElse(-1));
+            }
+        }
+        System.out.println("Benchmark complete after " + iter + " iters!");
+        System.out.println("Best times: " + times.stream().mapToDouble(Double::doubleValue).sorted().limit(3).average().orElse(-1));
+        System.out.println("Warmup sum: " + sum1);
+        System.out.println("Main sum: " + sum2);
     }
 }
