50ac92606078b05583dd68bea6c84d0234d6324c
Viktor Lofgren <vlofgren@marginalia.nu>
2025-08-15 03:11:57 +0200

    (index) Make metrics make more sense by normalizing them by query  budget

--- code/index/query/java/nu/marginalia/index/query/IndexSearchBudget.java
+++ code/index/query/java/nu/marginalia/index/query/IndexSearchBudget.java
@@ -4,9 +4,11 @@ package nu.marginalia.index.query;
 /** An execution time budget for index search operations. */
 public class IndexSearchBudget {
     private final long timeout;
+    private final long limitTime;
 
     public IndexSearchBudget(long limitTime) {
         this.timeout = System.currentTimeMillis() + limitTime;
+        this.limitTime = limitTime;
     }
 
     public boolean hasTimeLeft() { return System.currentTimeMillis() < timeout; }
