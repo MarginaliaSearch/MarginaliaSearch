9a789bf62ddbd1571da5778be0a5ae805528e73b
Viktor Lofgren <vlofgren@marginalia.nu>
2025-08-18 09:10:58 +0200

    (array) Fix broken test

--- code/libraries/array/test/nu/marginalia/array/NativeAlgosTest.java
+++ code/libraries/array/test/nu/marginalia/array/NativeAlgosTest.java
@@ -1,20 +1,31 @@
 package nu.marginalia.array;
 
 import nu.marginalia.ffi.LinuxSystemCalls;
-import nu.marginalia.uring.UringFileReader;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class NativeAlgosTest {
+    Path testFile;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        testFile = Files.createTempFile("NativeAlgosTest", ".dat");
+    }
+    @AfterEach
+    public void tearDown() throws IOException {
+        Files.deleteIfExists(testFile);
+    }
     @Test
     public void test() throws IOException {
-        LongArray array = LongArrayFactory.mmapForWritingShared(Path.of("/tmp/test"), 1024);
+        LongArray array = LongArrayFactory.mmapForWritingShared(testFile, 1024);
         for (int i = 0; i < 1024; i++) {
             array.set(i, i);
         }
@@ -22,7 +33,7 @@ public class NativeAlgosTest {
 
         var ms = Arena.global().allocate(512, 8);
 
-        int fd = LinuxSystemCalls.openDirect(Path.of("/tmp/test"));
+        int fd = LinuxSystemCalls.openDirect(testFile);
         int ret = LinuxSystemCalls.readAt(fd, ms, 512);
         System.out.println(ret);
         System.out.println(ms.byteSize());
@@ -37,14 +48,14 @@ public class NativeAlgosTest {
 
     @Test
     void testDirectFileReader() throws IOException {
-        LongArray array = LongArrayFactory.mmapForWritingShared(Path.of("/tmp/test"), 1024);
+        LongArray array = LongArrayFactory.mmapForWritingShared(testFile, 1024);
         for (int i = 0; i < 1024; i++) {
             array.set(i, i);
         }
         array.close();
 
 
-        try (var dfr = new DirectFileReader(Path.of("/tmp/test"))) {
+        try (var dfr = new DirectFileReader(testFile)) {
             LongArray array2 = LongArrayFactory.onHeapConfined(64);
             dfr.readAligned(array2, 0);
             for (int i = 0; i < array2.size(); i++) {
@@ -53,7 +64,7 @@ public class NativeAlgosTest {
         }
 
         var alignedBuffer = Arena.ofAuto().allocate(4096, 4096);
-        try (var dfr = new DirectFileReader(Path.of("/tmp/test"))) {
+        try (var dfr = new DirectFileReader(testFile)) {
             MemorySegment dest = Arena.ofAuto().allocate(504, 1);
             dfr.readUnaligned(dest, alignedBuffer, 8);
 
@@ -68,28 +79,4 @@ public class NativeAlgosTest {
         }
     }
 
-    @Test
-    void testAioFileReader() throws IOException {
-        LongArray array = LongArrayFactory.mmapForWritingShared(Path.of("/tmp/test"), 1024);
-        for (int i = 0; i < 1024; i++) {
-            array.set(i, i);
-        }
-        array.close();
-
-        try (var dfr = new UringFileReader(Path.of("/tmp/test"),  false)) {
-            MemorySegment buf1 = Arena.ofAuto().allocate(32, 8);
-            MemorySegment buf2 = Arena.ofAuto().allocate(16, 8);
-
-            dfr.read(List.of(buf1, buf2), List.of(0L, 8L));
-
-            for (int i = 0; i < buf1.byteSize(); i+=8) {
-                System.out.println(buf1.get(ValueLayout.JAVA_LONG, i));
-            }
-
-            for (int i = 0; i < buf2.byteSize(); i+=8) {
-                System.out.println(buf2.get(ValueLayout.JAVA_LONG, i));
-            }
-        }
-
-    }
 }
