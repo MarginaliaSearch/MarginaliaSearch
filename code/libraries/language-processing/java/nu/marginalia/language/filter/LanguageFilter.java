1cca16a58ec9237a10bc69915f435e5f9ca26e43
Viktor Lofgren <vlofgren@marginalia.nu>
2025-08-19 09:03:57 +0200

    (language) Simplify language filters

--- code/libraries/language-processing/java/nu/marginalia/language/filter/LanguageFilter.java
+++ code/libraries/language-processing/java/nu/marginalia/language/filter/LanguageFilter.java
@@ -1,12 +1,11 @@
 package nu.marginalia.language.filter;
 
+import com.github.jfasttext.JFastText;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import nu.marginalia.LanguageModels;
 import nu.marginalia.language.encoding.UnicodeRanges;
 import nu.marginalia.language.model.DocumentLanguageData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 import java.util.Set;
@@ -15,25 +14,27 @@ import java.util.Set;
 public class LanguageFilter {
 
     private final Set<String> permittedLanguages = Set.of("en", "sv");
-
-    private static final Logger logger = LoggerFactory.getLogger(LanguageFilter.class);
-
-    private final LanguagePredictionModel languagePredictionModel;
-
-    public Optional<String> predictLanguage(DocumentLanguageData dld) {
-        return languagePredictionModel
-                .predictLanguage(dld)
-                .filter(permittedLanguages::contains);
-    }
+    private final JFastText jft = new JFastText();
 
     @Inject
     public LanguageFilter(LanguageModels lm) {
-        try {
-            languagePredictionModel = new FasttextLanguagePredictionModel(lm);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        jft.loadModel(lm.fasttextLanguageModel.toString());
     }
+
+    public Optional<String> predictLanguage(DocumentLanguageData dld) {
+        String prediction = jft.predict(dld.text());
+
+        if (prediction.length() == "__label__??".length()) {
+            String isoCode = prediction.substring("__label__".length());
+
+            if (permittedLanguages.contains(isoCode)) {
+                return Optional.of(isoCode);
+            }
+        }
+
+        return Optional.empty();
+    }
+
 
     public boolean isBlockedUnicodeRange(String data) {
         for (var range: UnicodeRanges.values()) {
