3406b3c1e9acb896f5ac2535c5ef84760b2579f5
Viktor Lofgren <vlofgren@marginalia.nu>
2025-09-29 18:07:21 +0200

    (dom-sample) JSONL export of the dom sample data

--- code/processes/export-task-process/java/nu/marginalia/extractor/DomSampleDataExporter.java
+++ code/processes/export-task-process/java/nu/marginalia/extractor/DomSampleDataExporter.java
@@ -1,11 +1,17 @@
 package nu.marginalia.extractor;
 
 import com.google.inject.Inject;
+import nu.marginalia.domsample.db.DomSampleDb;
 import nu.marginalia.storage.FileStorageService;
 import nu.marginalia.storage.model.FileStorage;
 import nu.marginalia.storage.model.FileStorageId;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class DomSampleDataExporter {
     private final FileStorageService storageService;
@@ -16,9 +22,30 @@ public class DomSampleDataExporter {
         this.storageService = storageService;
     }
 
+    private record ExportModel(String domain, boolean acceptedPopover, String sample, List<String> requests) {
+        public ExportModel(DomSampleDb.Sample sample) {
+           this(sample.domain(),
+                   sample.acceptedPopover(),
+                   sample.sample(),
+                   Arrays.asList(StringUtils.split(sample.requests(), "\n")));
+        }
+
+    }
     public void export(FileStorageId destId) throws Exception {
         FileStorage destStorage = storageService.getStorage(destId);
-        // TODO:  Draw the rest of the owl
+
+        try (DomSampleDb domSampleDb = new DomSampleDb();
+             JsonLWriter<ExportModel> jsonLWriter =
+                     new JsonLWriter<>(destStorage.asPath(), "dom-samples", 100_000)) {
+            domSampleDb.forEachSample(sample -> {
+                try {
+                    jsonLWriter.write(new ExportModel(sample));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return true;
+            });
+        }
     }
 
 }
