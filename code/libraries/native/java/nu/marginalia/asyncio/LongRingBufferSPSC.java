db6deb4633e09c87d23666c09a38820410265345
Viktor Lofgren <vlofgren@marginalia.nu>
2026-01-08 15:05:50 +0100

    (uring) Ring buffer tweaks

--- code/libraries/native/java/nu/marginalia/asyncio/LongRingBufferSPSC.java
+++ code/libraries/native/java/nu/marginalia/asyncio/LongRingBufferSPSC.java
@@ -4,66 +4,70 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class LongRingBufferSPSC {
     final long[] items;
+    final int mask;
 
     final AtomicInteger writePos = new AtomicInteger(0);
     final AtomicInteger readPos = new AtomicInteger(0);
 
     public LongRingBufferSPSC(int len) {
         items = new long[len];
+        mask = items.length - 1;
+        assert (len & mask) == 0;
     }
 
     @SuppressWarnings("unchecked")
-    long take() {
+    public long take() {
         int rp;
-        for (;;) {
+        for (int iter = 0;;iter++) {
             while ((rp = readPos.get()) != writePos.get()) {
                 var ret = items[rp];
-                readPos.lazySet((rp + 1) % items.length);
+                readPos.lazySet((rp + 1) & mask);
                 return ret;
             }
-            Thread.yield();
+            if (iter > 1000)
+                Thread.yield();
         }
     }
 
-    int takeNonBlock(long[] buffer) {
+    public int takeNonBlock(long[] buffer) {
         final int rp = readPos.get();
         final int wp = writePos.get();
 
-        final int available = (wp - rp + items.length) % items.length;
+        final int available = (wp - rp + items.length) & mask;
         final int batchSize = Math.min(buffer.length, available);
 
         for (int i = 0; i < batchSize; i++) {
-            buffer[i] = items[(rp + i) % items.length];
+            buffer[i] = items[(rp + i) & mask];
         }
 
         if (batchSize > 0) {
-            readPos.lazySet((rp + batchSize) % items.length);
+            readPos.lazySet((rp + batchSize) & mask);
         }
 
         return batchSize;
     }
 
-    void put(long value) {
-        int nextPos = (writePos.get() + 1) % items.length;
-        while (nextPos == readPos.get()) {
-            Thread.yield();
+    public void put(long value) {
+        int nextPos = (writePos.get() + 1) & mask;
+        for (int iter = 0; nextPos == readPos.get(); iter++) {
+            if (iter > 1000) Thread.yield();
         }
         items[writePos.get()] = value;
         writePos.lazySet(nextPos);
     }
 
-    void put(long[] values, int n) {
+    public void put(long[] values, int n) {
         int i;
         final int wp = writePos.get();
         final int rp = readPos.get();
 
-        final int available = (rp - wp - 1 + items.length) % items.length;
+        final int available = (rp - wp - 1 + items.length) & mask;
         final int batchSize = Math.min(n, available);
 
         for (i = 0; i < batchSize; i++) {
             items[(wp+i)%items.length] = values[i];
         }
-        writePos.lazySet((wp + i) % items.length);
+        writePos.lazySet((wp + i) & mask);
 
         for (; i < n; i++)
             put(values[i]);
