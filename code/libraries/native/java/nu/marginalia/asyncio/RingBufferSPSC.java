b11d3acb0ef7d573ee7977974f3ae659d6ad8460
Viktor Lofgren <vlofgren@marginalia.nu>
2026-01-10 16:09:01 +0100

    Debug

--- code/libraries/native/java/nu/marginalia/asyncio/RingBufferSPSC.java
+++ code/libraries/native/java/nu/marginalia/asyncio/RingBufferSPSC.java
@@ -51,42 +51,22 @@ public class RingBufferSPSC<T> {
             ret[i++] = (T) items[nrp];
         }
 
-        readPos.lazySet((rp+i) % items.length);
+        if (!readPos.compareAndSet(rp, (rp+i) % items.length))
+            throw new IllegalStateException("read pos changed, multiple readers?");
 
         return i;
     }
 
-    @SuppressWarnings("unchecked")
-    public T tryTake() {
-        int rp;
-
-        if ((rp = readPos.get()) != writePos.get()) {
-            Object ret = items[rp];
-            if (readPos.compareAndSet(rp, (rp + 1) % items.length)) {
-                return (T) ret;
-            }
-        }
-
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    public T peek() {
-        int rp;
-        if ((rp = readPos.get()) != writePos.get()) {
-            return (T) items[rp];
-        }
-        return null;
-    }
-
     public boolean put(T item) {
-        int nextPos = (writePos.get() + 1) % items.length;
+        int wp = writePos.get();
+        int nextPos = (wp + 1) % items.length;
         if (nextPos == readPos.get()) {
             return false;
         }
-        items[writePos.get()] = item;
-        // single producer, safe
-        writePos.set(nextPos);
+        items[wp] = item;
+        if (!writePos.compareAndSet(wp, nextPos))
+            throw new IllegalStateException("write pos changed, multiple readers?");
+
         return true;
     }
 }
