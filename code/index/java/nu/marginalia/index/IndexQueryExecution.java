e3bae81e4df5dbb2d7c734a9566ab4c4d816e668
Viktor Lofgren <vlofgren@marginalia.nu>
2026-01-24 21:58:28 +0100

    (index) Reduce lookup batch size

--- code/index/java/nu/marginalia/index/IndexQueryExecution.java
+++ code/index/java/nu/marginalia/index/IndexQueryExecution.java
@@ -44,7 +44,7 @@ public class IndexQueryExecution {
     private static final int maxSimultaneousQueries = Integer.getInteger("index.maxSimultaneousQueries", 4);
     private static final Semaphore simultaneousRequests = new Semaphore(maxSimultaneousQueries);
 
-    private static final int lookupBatchSize = SkipListConstants.MAX_RECORDS_PER_BLOCK;
+    private static final int lookupBatchSize = 512;
 
     private static final ExecutorService threadPool = Executors.newCachedThreadPool();
 
