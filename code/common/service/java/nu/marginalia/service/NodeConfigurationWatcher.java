322f935cd7e39f8380a30081924367ad66a5e1af
Viktor Lofgren <vlofgren@marginalia.nu>
2026-03-09 18:17:19 +0100

    (service) Fix potential race condition in pollConfiguration()

--- code/common/service/java/nu/marginalia/service/NodeConfigurationWatcher.java
+++ code/common/service/java/nu/marginalia/service/NodeConfigurationWatcher.java
@@ -20,37 +20,45 @@ public class NodeConfigurationWatcher implements NodeConfigurationWatcherIf {
     public NodeConfigurationWatcher(HikariDataSource dataSource) {
         this.dataSource = dataSource;
 
+        // Perform the first poll synchronously so that getQueryNodes()
+        // returns valid data immediately after construction
+        queryNodes = pollQueryNodes();
+
         var watcherThread = new Thread(this::pollConfiguration, "Node Configuration Watcher");
         watcherThread.setDaemon(true);
         watcherThread.start();
     }
 
+    private List<Integer> pollQueryNodes() {
+        List<Integer> goodNodes = new ArrayList<>();
+
+        try (var conn = dataSource.getConnection()) {
+            var stmt = conn.prepareStatement("""
+                SELECT ID FROM NODE_CONFIGURATION
+                WHERE ACCEPT_QUERIES AND NOT DISABLED
+                """);
+            var rs = stmt.executeQuery();
+            while (rs.next()) {
+                goodNodes.add(rs.getInt(1));
+            }
+        }
+        catch (SQLException ex) {
+            logger.error("Error polling node configuration", ex);
+        }
+
+        return goodNodes;
+    }
+
     private void pollConfiguration() {
         for (;;) {
-            List<Integer> goodNodes = new ArrayList<>();
-
-            try (var conn = dataSource.getConnection()) {
-                var stmt = conn.prepareStatement("""
-                    SELECT ID FROM NODE_CONFIGURATION
-                    WHERE ACCEPT_QUERIES AND NOT DISABLED
-                    """);
-                var rs = stmt.executeQuery();
-                while (rs.next()) {
-                    goodNodes.add(rs.getInt(1));
-                }
-            }
-            catch (SQLException ex) {
-                logger.error("Error polling node configuration", ex);
-            }
-
-            queryNodes = goodNodes;
-
             try {
                 TimeUnit.SECONDS.sleep(10);
             }
             catch (InterruptedException ex) {
                 return;
             }
+
+            queryNodes = pollQueryNodes();
         }
     }
 
