42d5cbea3503d9f7cb88814257a8c7e670c56074
Viktor Lofgren <vlofgren@marginalia.nu>
2026-03-11 09:51:21 +0100

    (nsfw)  Remove redundant exception

--- code/functions/nsfw-filter/java/nu/marginalia/nsfw/document/NsfwDocumentFilter.java
+++ code/functions/nsfw-filter/java/nu/marginalia/nsfw/document/NsfwDocumentFilter.java
@@ -7,7 +7,6 @@ import nu.marginalia.language.model.DocumentSentence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
