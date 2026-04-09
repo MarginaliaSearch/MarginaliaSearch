cda3d03596c795af790f6f003b47b6b99dfb577c
Viktor Lofgren <vlofgren@marginalia.nu>
2026-04-09 14:15:31 +0200

    (headless) Implement basic pooling and proper termination criteria

--- code/tools/headlesschrome/java/nu/marginalia/headless/ChromeDriverManager.java
+++ code/tools/headlesschrome/java/nu/marginalia/headless/ChromeDriverManager.java
@@ -3,33 +3,110 @@ package nu.marginalia.headless;
 import nu.marginalia.WmsaHome;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ChromeDriverManager {
     private static final String CHROME_PATH = "/usr/bin/chromium";
+    private static final Logger logger = LoggerFactory.getLogger(ChromeDriverManager.class);
 
-    private final ChromeDriver driver;
+    private ChromeOptions vanillaOptions;
+    private ChromeOptions extensionOptions;
 
-    public ChromeDriverManager() {
-        ChromeOptions options = chromeOptions();
-        driver = new ChromeDriver(options);
-    }
+    private final ArrayBlockingQueue<DriverHolder> vanillaDriverHolders;
+    private final ArrayBlockingQueue<DriverHolder> extensionDriverHolders;
 
-    // FIXME: We should pool these
-    public ChromeDriver getDriver() {
-        return driver;
-    }
+    public ChromeDriverManager(int queueSize) {
+        vanillaDriverHolders = new ArrayBlockingQueue<>(queueSize);
+        extensionDriverHolders = new ArrayBlockingQueue<>(queueSize);
 
-    private static ChromeOptions chromeOptions() {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary(CHROME_PATH);
-        options.addArguments(
+        var baseOptions = new ChromeOptions();
+        baseOptions.setBinary(CHROME_PATH);
+        baseOptions.addArguments(
                 "--headless=new",
                 "--no-sandbox",
                 "--disable-dev-shm-usage",
                 "--disable-gpu",
-                "--window-size=1024,768",
                 "--user-agent=" + WmsaHome.getUserAgent().uaString()
         );
-        return options;
+
+        vanillaOptions = new ChromeOptions().merge(baseOptions).addArguments("--window-size=1024,768");
+        extensionOptions = new ChromeOptions().merge(baseOptions).addArguments("--load-extension=/dom-export");
+
+        for (int i = 0; i < 4; i++) {
+            vanillaDriverHolders.add(new DriverHolder(new ChromeDriver(vanillaOptions), vanillaDriverHolders));
+            extensionDriverHolders.add(new DriverHolder(new ChromeDriver(extensionOptions), extensionDriverHolders));
+        }
     }
+
+    public DriverHolder getVanillaDriver(Duration timeout) throws InterruptedException {
+        var holder = vanillaDriverHolders.poll(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        if (holder.isDead()) {
+            try {
+                holder.driver.quit();
+            }
+            catch (Exception ex) {}
+            holder = new DriverHolder(new ChromeDriver(vanillaOptions), vanillaDriverHolders);
+        }
+        return holder;
+    }
+
+
+
+    public DriverHolder getExtensionDriver(Duration timeout) throws InterruptedException {
+        var holder = extensionDriverHolders.poll(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        if (holder.isDead()) {
+            try {
+                holder.driver.quit();
+            }
+            catch (Exception ex) {}
+            holder = new DriverHolder(new ChromeDriver(extensionOptions), extensionDriverHolders);
+        }
+        return holder;
+    }
+
+    public class DriverHolder implements AutoCloseable {
+        private final ChromeDriver driver;
+        private final ArrayBlockingQueue<DriverHolder> queue;
+        private AtomicInteger uses = new AtomicInteger(0);
+        private AtomicBoolean dead = new AtomicBoolean(false);
+
+        DriverHolder(ChromeDriver driver, ArrayBlockingQueue<DriverHolder> queue) {
+            this.driver = driver;
+            this.queue = queue;
+        }
+
+        public ChromeDriver get() {
+           return driver;
+        }
+
+        public int uses() {
+            return uses.get();
+        }
+
+        public boolean isDead() {
+            return dead.get() || uses.get() >= 10;
+        }
+
+        public void close() {
+            uses.incrementAndGet();
+
+            try {
+                driver.navigate().to("about:blank");
+                driver.manage().deleteAllCookies();
+                queue.add(this);
+            }
+            catch (Exception ex) {
+                logger.info("Stuck driver, killing");
+                dead.set(true);
+            }
+        }
+    }
+
 }
