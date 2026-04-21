26c011d7458a14220e55e949eb9adbdf75e17f59
Viktor Lofgren <vlofgren@marginalia.nu>
2026-04-21 11:05:46 +0200

    (service) Add curl-free health check helper

---
+++ code/common/service/java/nu/marginalia/service/ServiceHealthCheckMain.java
@@ -0,0 +1,53 @@
+package nu.marginalia.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+/**
+ * Curl-free health check helper for services.
+ *
+ * Can be installed like
+ *
+ *     healthcheck:
+ *       test: ["CMD-SHELL", "JAVA_TOOL_OPTIONS=\"\" JDK_JAVA_OPTIONS=\"\" java -Xmx64m -cp @/app/jib-classpath-file nu.marginalia.service.ServiceHealthCheckMain http://localhost:80/internal/ready"]
+ *       interval: 1m30s
+ *       timeout: 10s
+ *       retries: 3
+ *       start_period: 300s
+ *       start_interval: 15s
+ *
+ */
+public class ServiceHealthCheckMain {
+    public static void main(String[] args) {
+        String url = args[0];
+
+        System.out.println("Health check: " + url);
+
+        HttpClient client = HttpClient.newBuilder()
+                .executor(Executors.newSingleThreadExecutor())
+                .build();
+
+        try (client) {
+            int code = client.send(HttpRequest.newBuilder()
+                        .uri(new URI(args[0]))
+                        .timeout(Duration.ofSeconds(5))
+                .build(), HttpResponse.BodyHandlers.discarding())
+                    .statusCode();
+            if (code < 300) {
+                System.out.println("Health check OK");
+                System.exit(0);
+            }
+            else {
+                System.err.println("Health check failed: " + code);
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            System.err.println("Health check failed: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
