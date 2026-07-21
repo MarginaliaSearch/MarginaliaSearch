684cbaecb65a1654b0cf5dab221da0cfa94573da
Viktor Lofgren <vlofgren@marginalia.nu>
2026-07-21 10:58:54 +0200

    (index) Fix handling of unranked queries when index is not available

--- code/index/java/nu/marginalia/index/IndexGrpcService.java
+++ code/index/java/nu/marginalia/index/IndexGrpcService.java
@@ -180,6 +180,7 @@ public class IndexGrpcService
                             .build());
 
                     responseObserver.onCompleted();
+                    return;
                 }
 
                 CombinedIndexReader index = indexReference.get();
